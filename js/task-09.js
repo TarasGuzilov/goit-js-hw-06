8fb357a034e41966ff46f3c7ccc9e382826d47bf
TarasGuzilov <tarasvakcina@rambler.ru>
2022-02-27 12:40:00 +0300

    Задача № 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', () => {
+  bodyRef.style = `background-color: ${getRandomHexColor()}`;
+  colorName.textContent = getRandomHexColor();
+});
