fc9c3060dca6f311c3efc7ca2e28c0dcc0f7e5a3
TarasGuzilov <tarasvakcina@rambler.ru>
2022-02-27 18:33:11 +0300

    Частичное решение задачи № 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,58 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesRef = document.querySelector('#boxes');
+const inputRef = document.querySelector('#controls > input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+
+console.log(boxesRef);
+console.log(inputRef);
+console.log(btnCreate);
+console.log(btnDestroy);
+
+const firstBoxSize = 30
+const newBoxes = [];
+
+btnCreate.addEventListener('click', () => {
+  createBoxes(inputRef.value)
+});
+
+btnDestroy.addEventListener('click', () => {
+  const array = boxesRef.querySelectorAll('div');
+  array.forEach(element => {
+    element.remove();
+  });
+  // boxesRef.innerHTML = '';
+
+});
+
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const element = document.createElement('div');
+    element.style.width = `${firstBoxSize + i * 10}px`;
+    element.style.height = `${firstBoxSize + i * 10}px`;
+    element.style.backgroundColor = getRandomHexColor();
+    newBoxes.push(element);
+  }
+  console.log(newBoxes);
+  boxesRef.append(...newBoxes);
+  
+}
+
+
+/* <body>
+    <p><a href="index.html">Go back</a></p>
+
+    <div id="controls">
+      <input type="number" min="1" max="100" step="1" />
+      <button type="button" data-create>Create</button>
+      <button type="button" data-destroy>Destroy</button>
+    </div>
+
+    <div id="boxes"></div>
+
+    <script src="js/task-10.js" type="module"></script>
+  </body> */
